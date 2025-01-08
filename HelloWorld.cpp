a0592c2879a070a183367157edc97d2d4d62510e
KKJ1000 <kkj1733n@gmail.com>
2025-01-08 21:00:18 +0900

    [Feat] Hello, World!
    C++ 첫걸음

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  //iostream 헤더를 포함(include)
+using namespace std; //네임스페이스 ex) std::cout
+
+//C++ 코드에는 반드시 main이라는 이름을 가지고 있는 함수가 존재해야한다.
+
+//int : 반환형, main : 함수명
+int main() // entry point
+{
+	// 입출력 연산자 (<<, >>)
+	cout << "Hello, Wolrd" << endl; // endl : 줄바꿈
+	return 0; // 반환 값
+}
